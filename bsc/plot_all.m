6f3ccd3c0536d1bfae33d7095876ed6349f6490a
Hengjie Yang <dragonyhj@icloud.com>
2022-10-28 09:34:19 -0700

    Changed plot_all in BSC folder

--- bsc/plot_all.m
+++ bsc/plot_all.m
@@ -1,4 +1,10 @@
-function [Ns ach_rcu ach_dt ach_gal conv normapx_val] = plot_all(delta, epsil);
+function [Ns ach_rcu ach_dt ach_gal conv normapx_val] = plot_all(delta, epsil)
+
+set(0,'DefaultTextFontName','Times','DefaultTextFontSize',16,...
+    'DefaultAxesFontName','Times','DefaultAxesFontSize',14,...
+    'DefaultLineLineWidth',1,'DefaultLineMarkerSize',7.75);
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
 
 if (nargin < 1) || isempty(delta)
 	delta = 0.11;
@@ -20,20 +26,20 @@ ach_dt = dt_ach(Ns, delta, epsil);
 
 % Compute RCU bound
 ach_rcu = [];
-for idx=1:length(Ns);
+for idx=1:length(Ns)
 	n=Ns(idx);
 	% provide good bracket for speeding up
 	plow = max(ach_gal(idx),ach_dt(idx));
 	pup = conv(idx);
 	ach_rcu(idx) = rcu_ach(n, delta, epsil, plow, pup);
-end;
+end
 
 
 %% normal approximation
 figure;
 plot(Ns_cap, Capr, 'r--', Ns, conv./Ns, 'r', Ns, normapx_val./Ns, 'k-', Ns, ach_rcu./Ns, 'b', Ns, ach_dt./Ns, 'b--', ...
 	Ns, ach_gal./Ns, 'b-.');
-xlabel('Blocklen, n'); ylabel('Rate, R'); ylim([0 1.05*cap]);
+xlabel('Blocklength, $n$', 'Interpreter','latex'); ylabel('Rate, $R$', 'Interpreter','latex'); ylim([0 1.05*cap]);
 title(sprintf('Bounds for the BSC(%g), P_{e,max} = %g', delta, epsil));
 legend('Capacity', 'Converse',  'Normal approximation', 'RCU achievability', 'DT achievability', 'Gallager achievability', 'Location', 'SouthEast');
 grid on
